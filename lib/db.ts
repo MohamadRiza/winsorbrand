e856664d265fa1902e60b882e891c642a52789bd
MohamadRiza <rawufdeenriza@gmail.com>
2026-06-09 12:57:04 +0530

    CI/CD testing 1

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,18 @@
 // lib/db.ts
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
-
-if (!MONGODB_URI) {
-  throw new Error('Please define MONGODB_URI in .env.local');
-}
-
 // Cache connection across hot-reloads in dev (same trick as MERN)
 let cached = (global as any).mongoose || { conn: null, promise: null };
 (global as any).mongoose = cached;
 
 export async function connectDB() {
   if (cached.conn) return cached.conn;
+
+  const MONGODB_URI = process.env.MONGODB_URI as string;
+
+  if (!MONGODB_URI) {
+    throw new Error('Please define MONGODB_URI in .env.local');
+  }
 
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
